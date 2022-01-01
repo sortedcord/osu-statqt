ff1ca19c919783d9965bac704825e0a5bc00c47a
Aditya Gupta <aditsuyag@gmail.com>
2022-01-01 09:28:41 +0530

    Overhauled Recent Scores

--- functions.py
+++ functions.py
@@ -36,14 +36,15 @@ def get_time_elapsed(d1):
     dif = d2 - d1
 
     hours = (dif.seconds)//(60*60)
+    minutes = (dif.seconds)//(60)
 
     if dif.days >= 1:
         return f"{dif.days} days ago"
     elif dif.days < 1 and hours >= 1:
         return f"{hours} hours ago"
-    elif (dif.days,hours) < (1,1) and dif.minutes >= 1:
-        return f"{hours} hours ago"
-    elif (dif.days,hours,dif.minutes) < (1,1,1):
+    elif (dif.days,hours) < (1,1) and minutes >= 1:
+        return f"{minutes} minutes ago"
+    elif (dif.days,hours,minutes) < (1,1,1):
         return f"A few seconds ago"
 
     
--- tabs.py
+++ tabs.py
@@ -1,6 +1,10 @@
 from PyQt5 import QtCore, QtWidgets
+from ossapi import enums
 
 from Components.recent_activity import RecentActivityItem
+from Components.recent_scores import RecentScoreItem
+
+import ossapi
 
 
 
@@ -126,7 +130,7 @@ QScrollBar::add-page:horizontal, QScrollBar::sub-page:horizontal
 
 }
 
-        """)
+""")
 
 
 class RecentScoreTab(QtWidgets.QWidget):
@@ -136,16 +140,18 @@ class RecentScoreTab(QtWidgets.QWidget):
         self.setupConnections(mainWindow)
 
     def setupConnections(self, mainWindow):
-        try:
-            for recent_score in mainWindow.config.api.user_scores(mainWindow.default_user_class.id, 'recent'):
-                widget = QtWidgets.QLabel()
-                widget.setText(recent_score.beatmapset.title)
-                self.verticalLayout_2.addWidget(widget)
-        except:
-            widget = QtWidgets.QLabel()
-            widget.setText(
-                f"Could not find any more recent scores for {mainWindow.default_user_class.username}")
+        
+        for recent_score in mainWindow.config.api.user_scores(mainWindow.default_user_class.id, 'recent', "1", limit=10):
+            print("grade",recent_score.rank)
+            print(recent_score.rank == enums.Grade.A)
+            print("grade",type(recent_score.rank))
+            widget = RecentScoreItem(recent_score)
             self.verticalLayout_2.addWidget(widget)
+        # except:
+        #     widget = QtWidgets.QLabel()
+        #     widget.setText(
+        #         f"Could not find any more recent scores for {mainWindow.default_user_class.username}")
+        #     self.verticalLayout_2.addWidget(widget)
 
     def setupUi(self):
         self.setObjectName("self")
@@ -168,4 +174,93 @@ class RecentScoreTab(QtWidgets.QWidget):
         self.verticalLayout_2.setObjectName("verticalLayout_2")
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
         self.verticalLayout.addWidget(self.scrollArea)
+        self.scrollArea.setStyleSheet("""
+        QScrollBar:vertical {
+	border: none;
+    background: rgb(45, 45, 68);
+    width: 14px;
+    margin: 15px 0 15px 0;
+	border-radius: 0px;
+ }
+
+/*  HANDLE BAR VERTICAL */
+QScrollBar::handle:vertical {	
+	background-color: #ac396d;
+	min-height: 30px;
+	border-radius: 0px;
+}
+QScrollBar::handle:vertical:hover{	
+	background-color: rgb(255, 102, 171);
+}
+QScrollBar::handle:vertical:pressed {	
+	background-color: rgb(255, 102, 171);
+}
+
+/* BTN TOP - SCROLLBAR */
+QScrollBar::sub-line:vertical {
+	border: none;
+	background-color: rgb(59, 59, 90);
+	height: 15px;
+	border-top-left-radius: 0px;
+	border-top-right-radius: 0px;
+	subcontrol-position: top;
+	subcontrol-origin: margin;
+}
+QScrollBar::sub-line:vertical:hover {	
+	background-color: rgb(255, 102, 171);
+}
+QScrollBar::sub-line:vertical:pressed {	
+	background-color: rgb(255, 102, 171);
+}
+
+/* BTN BOTTOM - SCROLLBAR */
+QScrollBar::add-line:vertical {
+	border: none;
+	background-color: rgb(59, 59, 90);
+	height: 15px;
+	border-bottom-left-radius: 0px;
+	border-bottom-right-radius: 0px;
+	subcontrol-position: bottom;
+	subcontrol-origin: margin;
+}
+QScrollBar::add-line:vertical:hover {	
+	background-color: rgb(255, 0, 127);
+}
+QScrollBar::add-line:vertical:pressed {	
+	background-color: rgb(255, 102, 171);
+}
+
+/* RESET ARROW */
+QScrollBar::up-arrow:vertical, QScrollBar::down-arrow:vertical {
+	background: none;
+}
+QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical {
+	background: none;
+}
+
+
+
+/* HORIZONTAL SCROLLBAR - HOMEWORK */
+QScrollBar:horizontal {
+   
+}
+QScrollBar::handle:horizontal {
+    
+}
+QScrollBar::add-line:horizontal {
+    
+}
+QScrollBar::sub-line:horizontal {
+    
+}
+QScrollBar::up-arrow:horizontal, QScrollBar::down-arrow:horizontal
+{
+
+}
+QScrollBar::add-page:horizontal, QScrollBar::sub-page:horizontal
+{
+
+}
+
+        """)
         
