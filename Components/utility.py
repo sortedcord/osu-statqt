f4895fd3abf959ca222d4809ea7c094050b5474f
Aditya Gupta <aditsuyag@gmail.com>
2022-01-12 13:46:45 +0530

    Reduced Code for UserTab

--- Components/utility.py
+++ Components/utility.py
@@ -19,3 +19,43 @@ class MsgBox(QMessageBox):
         self.setText(text)
 
         _ = self.exec_()
+
+class CustomSizePolicy(QSizePolicy):
+    def __init__(self, horizontalPolicy, verticalPolicy, parent):
+        super().__init__()
+
+        self.setHorizontalPolicy(horizontalPolicy)
+        self.setVerticalPolicy(verticalPolicy)
+
+        self.setHorizontalStretch(0)
+        self.setVerticalStretch(0)
+
+        self.setHeightForWidth(parent.sizePolicy().hasHeightForWidth())
+
+        parent.setSizePolicy(self)
+
+
+class CustomVLayout(QVBoxLayout):
+    def __init__(self, parent, margins=(0,0,0,0), spacing=0):
+        super().__init__(parent)
+
+        # Set Margins
+        _1, _2, _3, _4 = margins
+        self.setContentsMargins(_1,_2,_3,_4)
+
+        self.setSpacing(spacing)
+
+
+class CustomHLayout(QHBoxLayout):
+    def __init__(self, parent, margins=(0,0,0,0), spacing=0):
+        super().__init__(parent)
+
+        # Set Margins
+        _1, _2, _3, _4 = margins
+        self.setContentsMargins(_1,_2,_3,_4)
+
+        self.setSpacing(spacing)
+
+
+
+
