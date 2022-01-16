a955a448cb3468969585c91c5991c62246ebe49d
Aditya Gupta <aditsuyag@gmail.com>
2022-01-16 10:38:55 +0530

    (fix #36): All Events show Correct gamemodes

--- OsuStatQt/tabs/RecentActivityTab.py
+++ OsuStatQt/tabs/RecentActivityTab.py
@@ -11,78 +11,79 @@ from loguru import logger
 import ossapi
 
 class RecentActivityTab(QWidget):
-    def __init__(self, mainWindow):
-        super().__init__()
+	def __init__(self, mainWindow):
+		super().__init__()
 
-        self.setupUi(mainWindow)
-        self.offset = 0
-        self.setupConnections(mainWindow)
+		self.setupUi(mainWindow)
+		self.offset = 0
+		self.setupConnections(mainWindow)
 
-    def show_more_clicked(self, mainWindow):
-        self.verticalLayout_2.removeWidget(self.show_more_button)
-        self.offset += 20
-        self.setupConnections(mainWindow)
+	def show_more_clicked(self, mainWindow):
+		self.verticalLayout_2.removeWidget(self.show_more_button)
+		self.offset += 20
+		self.setupConnections(mainWindow)
 
-    def setupConnections(self, mainWindow):
-        # print(mainWindow.config.api)
-        # exit()
+	def setupConnections(self, mainWindow):
+		# print(mainWindow.config.api)
+		# exit()
 
-        logger.info(f"offset set to: {self.offset}")
-        _count = 0
-        for recent_activity in mainWindow.config.api.user_recent_activity(user_id=mainWindow.config.default_user.id, limit=mainWindow.config.panel_items, offset=self.offset):
-            QApplication.processEvents()
-            widget = RecentActivityItem(mainWindow, recent_activity)
-            self.verticalLayout_2.addWidget(widget)
-            _count += 1
-        
-        logger.debug(f"Created {_count} RecentActivity Cards")
-        self.verticalLayout_2.addWidget(self.show_more_button)
-        self.show_more_button.clicked.connect(lambda: self.show_more_clicked(mainWindow))
+		logger.info(f"offset set to: {self.offset}")
+		_count = 0
+		for recent_activity in mainWindow.config.api.user_recent_activity(user_id=mainWindow.config.default_user.id, limit=mainWindow.config.panel_items, offset=self.offset):
+			QApplication.processEvents()
+			widget = RecentActivityItem(mainWindow, recent_activity)
+			self.verticalLayout_2.addWidget(widget)
+			_count += 1
+		
+		logger.debug(f"Created {_count} RecentActivity Cards")
+		self.verticalLayout_2.addWidget(self.show_more_button)
+		self.show_more_button.clicked.connect(lambda: self.show_more_clicked(mainWindow))
 
 
-    def setupUi(self, mainWindow):
-        self.resize(778, 352)
-        self.verticalLayout = QVBoxLayout(self)
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.scrollArea = QScrollArea(self)
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollAreaWidgetContents = QWidget()
-        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 776, 350))
-        self.verticalLayout_2 = QVBoxLayout(
-            self.scrollAreaWidgetContents)
-        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
-        self.verticalLayout.addWidget(self.scrollArea)
+	def setupUi(self, mainWindow):
+		self.resize(778, 352)
+		self.verticalLayout = QVBoxLayout(self)
+		self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+		self.scrollArea = QScrollArea(self)
+		self.scrollArea.setWidgetResizable(True)
+		self.scrollAreaWidgetContents = QWidget()
+		self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 776, 350))
+		self.verticalLayout_2 = QVBoxLayout(
+			self.scrollAreaWidgetContents)
+		self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+		self.verticalLayout.addWidget(self.scrollArea)
 
-        self.show_more_button = QPushButton(self.scrollAreaWidgetContents)
-        self.show_more_button.setText("SHOW MORE")
-        
-        
-        self.scrollArea.setStyleSheet(scrollbar_style)
-        self.setStyleSheet("""background-color: #2A2327;
-                           color:rgb(255,255,255);
-                           font: 63 9pt "Torus Pro"; 
-                           border: none;""")
+		self.show_more_button = QPushButton(self.scrollAreaWidgetContents)
+		self.show_more_button.setText("SHOW MORE")
+		
+		
+		self.scrollArea.setStyleSheet(scrollbar_style)
+		self.setStyleSheet("""background-color: #2A2327;
+						   color:rgb(255,255,255);
+						   font: 63 9pt "Torus Pro"; 
+						   border: none;""")
 
-        self.show_more_button.setStyleSheet("""
-            QPushButton {
-                background:rgb(85,68,76);
-                border:none;
-                border-radius: 14px;
-                padding: 7px;
-                max-width:140px;
-                font: 63 9pt \"Torus Pro SemiBold\";
-                margin-top: 10px;
+		self.show_more_button.setStyleSheet("""
+			QPushButton {
+				background:rgb(85,68,76);
+				border:none;
+				border-radius: 14px;
+				padding: 7px;
+				max-width:140px;
+				font: 63 9pt \"Torus Pro SemiBold\";
+				margin-top: 10px;
 
-            }
-            QPushButton:hover {
-                background: rgb(112,92,101);
-            }
-        """)
+			}
+			QPushButton:hover {
+				background: rgb(112,92,101);
+			}
+		""")
 
-        #Add to mainMenu
-        try:
-            mainWindow.verticalLayout_3.addWidget(self)
-        except: pass
+		#Add to mainMenu
+		try:
+			mainWindow.verticalLayout_3.addWidget(self)
+		except: pass
+
 
 class RecentActivityItem(QWidget):
 	def __init__(self, mainWindow, activity):
@@ -123,11 +124,15 @@ class RecentActivityItem(QWidget):
 		self.datetime_label = CustomLabel(self, color="rgb(129,112,121)", text=get_time_elapsed(activity.created_at))
 		self.horizontalLayout.addWidget(self.datetime_label)
 
+		try:
+			self.gamemode_label.setText(f"({str(activity.mode).replace('GameMode.','').replace('STD','osu!')})")
+		except: pass
+
 		if type(activity) == ossapi.models.RankEvent:
 			self.event_description_label.setText(f" achieved rank # {activity.rank} on ")
 			self.event_type.setText(f"{activity.beatmap.title} ")
-			self.gamemode_label.setText("(osu!)")
-            
+			
+			
 			self.rank_score_label.setPixmap(QtGui.QPixmap(f"{mainWindow.assetpath}/Rank_Grades/{str(activity.scoreRank).replace('X','SS')}.png"))
 
 		elif type(activity) == ossapi.models.RankLostEvent:
